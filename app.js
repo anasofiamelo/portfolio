9fe78d99b56fcdab9830fb1feb14e7399d3a023a
Ana Sofia <sofiamelod@gmail.com>
2022-10-10 17:31:25 -0300

    adding image to hero-section, new tags to work card projects and att the projects

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const moon = document.querySelector('.fas.fa-moon')
-const sun = document.querySelector('.far.fa-sun')
-const container = document.querySelector('body')
+const moon = document.querySelector(".fas.fa-moon");
+const sun = document.querySelector(".far.fa-sun");
+const container = document.querySelector("body");
 
-moon.onclick = function() {
-    container.classList.toggle('dark-mode')
-    sun.classList.remove('none')
+moon.onclick = function () {
+  container.classList.toggle("dark-mode");
+  sun.classList.remove("none");
+};
+sun.onclick = function () {
+  container.classList.toggle("dark-mode");
+  moon.classList.remove("none");
+};
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  var btn = document.querySelector(".up-btn");
+  btn.classList.toggle("sticky", window.scrollY > 0);
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//hamburger
+let btnMobile = document.querySelector("#btn-mobile");
+btnMobile.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  let nav = document.querySelector("#nav");
+  nav.classList.toggle("active");
 }
-sun.onclick = function() {
-    container.classList.toggle('dark-mode')
-    moon.classList.remove('none')
-}
 
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
-    var btn = document.querySelector('.up-btn')
-    btn.classList.toggle('sticky', window.scrollY > 0)
-    header.classList.toggle('sticky', window.scrollY > 0)
-})
-
-//hamburger 
-let btnMobile = document.querySelector('#btn-mobile')
-btnMobile.addEventListener('click', toggleMenu);
-
-function toggleMenu(){
-    let nav = document.querySelector('#nav')
-    nav.classList.toggle('active')
-}
+const image = document.querySelector(".hero-section_image");
+image.onclick = () => console.log(image);
+// console.log(image);
